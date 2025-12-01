cd177a57bebae6869753d33000bbae3fe74b8a69
popatsargar <sargarpopat219@gmail.com>
2025-12-02 00:27:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+document.getElementById("button").addEventListener("click", function () {
+    const select = document.getElementById("colorSelect");
+
+    
+    if (select.selectedIndex !== -1) {
+        select.remove(select.selectedIndex);
+    }
+});
